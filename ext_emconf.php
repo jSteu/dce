dd18314c7cffa2a6f12253eb1c62c390925498f2
Johannes Steu <js@johannessteu.de>
2015-06-22 13:28:22 +0200

    [TASK] Add composer.json and TYPO3 7.2 compatibility

--- ext_emconf.php
+++ ext_emconf.php
@@ -30,7 +30,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'CGLcompliance_note' => '',
 	'constraints' => array(
 		'depends' => array(
-			'typo3' => '6.2.0-7.1.99',
+			'typo3' => '6.2.0-7.3.99',
 		),
 		'conflicts' => array(
 		),
